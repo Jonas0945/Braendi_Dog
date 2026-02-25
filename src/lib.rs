5df5dfafa30e796d202937289d7fa18185096417
samOOOQ <david.samudra@bassl.com>
2026-02-25 19:19:03 +0100

    Reorganize module imports and update enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
 // src/lib.rs
-pub mod game;
-pub mod ui;
 pub mod ai;
+pub mod game;
 pub mod net;
+pub mod ui;
 pub use net::*;
 //pub mod bin;
-pub use game::game::{Game, DogGame};
-pub use game::color::Color;
-pub use game::piece::Piece;
 pub use game::action::{Action, ActionKind};
+pub use game::card::Card;
+pub use game::color::Color;
+pub use game::game::GameVariant;
+pub use game::game::{DogGame, Game};
+pub use game::piece::Piece;
 use serde::{Deserialize, Serialize};
 pub use ui::render;
-pub use game::card::Card;          
-pub use game::game::GameVariant;
-
-
 
 //Aktionen, die SPieler versuchen können
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum ClientNachricht {
     beitritt,
     make_play,
-    quit
+    quit,
 }
 //informiert Client um Gui zu aktualisieren
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(tag = "type", content = "data")]
 pub enum ServerNachrich{
     Fehler(String),
-   // Info(String),
     State(Game),
+    Welcome(usize), 
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum BeginGameMesage {
-    ErstelleSpiel {variant: GameVariant, player_name: String},
+    ErstelleSpiel {
+        variant: GameVariant,
+        player_name: String,
+    },
 
-    SpielBeitreten {player_name: String},
+    SpielBeitreten {
+        player_name: String,
+    },
 }
-
